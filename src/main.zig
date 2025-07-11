ac6c623b1354b4be2aa516aa90370c774104d246
Ryan Tuttle <rtuttle@bitwarden.com>
2025-07-11 18:17:35 -0400

    builds without issue, good starting point

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,7 @@
 const std = @import("std");
 
-extern fn c_main() c_int;
+extern fn c_main() void;
 
 pub fn main() void {
-    const exit_code = c_main();
-    std.process.exit(@intCast(exit_code));
+    c_main();
 }
